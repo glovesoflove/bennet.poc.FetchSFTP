860e31bcbfe6abc16ee53668ee642ccc9f212720
Gary Grice <ggricewu@gmail.com>
2021-07-30 09:48:28 +0200

    Add project files.

---
+++ bennet.poc.FetchSFTP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bennet.poc.FetchSFTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
